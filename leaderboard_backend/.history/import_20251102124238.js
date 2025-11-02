5bcaa9b462041b0dafc30a1aa620611162ec522a
ramya1106 <ramya181106@gmail.com>
2025-11-02 14:03:23 +0530

    Included backend

---
+++ leaderboard_backend/.history/import_20251102124238.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const Database = require("sqlite3");
+
+const db = new Database("students.db");
+const data = JSON.parse(fs.readFileSync("data.json", "utf8"));
+
+const insert = db.prepare(`
+  INSERT INTO students (
+    sno, name, email, profileURL, profileStatus, redemptionStatus,
+    skillBadges, arcadeGames, totalCompletion, skillBadgesList, arcadeGamesList
+  ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+`);
+
+db.transaction(() => {
+  data.forEach(student => {
+    insert.run(
+      student.sno,
+      student.name,
+      student.email,
+      student.profileURL,
+      student.profileStatus,
+      student.redemptionStatus,
+      student.skillBadges,
+      student.arcadeGames,
+      student.totalCompletion,
+      JSON.stringify(student.skillBadgesList),
+      JSON.stringify(student.arcadeGamesList)
+    );
+  });
+})();
+
+console.log("✅ JSON data inserted successfully!");
---
+++ leaderboard_backend/.history/server_20251102124834.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const sqlite3 = require("sqlite3");
+
+const app = express();
+app.use(express.json());
+
+// connect DB
+const db = new sqlite3.Database("students.db");
+
+// GET all students
+app.get("/students", (req, res) => {
+  db.all("SELECT * FROM students", [], (err, rows) => {
+    if (err) return res.status(500).json({ error: err.message });
+
+    const formatted = rows.map(row => ({
+      ...row,
+      skillBadgesList: JSON.parse(row.skillBadgesList),
+      arcadeGamesList: JSON.parse(row.arcadeGamesList)
+    }));
+
+    res.json(formatted);
+  });
+});
+
+// GET student by ID
+app.get("/students/:id", (req, res) => {
+  const { id } = req.params;
+
+  db.get("SELECT * FROM students WHERE sno = ?", [id], (err, row) => {
+    if (err) return res.status(500).json({ error: err.message });
+    if (!row) return res.status(404).json({ message: "Student not found" });
+
+    row.skillBadgesList = JSON.parse(row.skillBadgesList);
+    row.arcadeGamesList = JSON.parse(row.arcadeGamesList);
+
+    res.json(row);
+  });
+});
+
+// Start server
+app.listen(3000, () => console.log("Server running on port https://localhost/3000"));
